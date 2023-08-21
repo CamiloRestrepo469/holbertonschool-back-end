01d70e02fbec9b4e07df58c24e496b5466ae026b
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-08-21 12:56:43 -0500

    new file

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
-
+"""import"""
+import json
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        user_response = requests.get(user_url)
-        todos_response = requests.get(todos_url)
-
-        if user_response.status_code != 200 or todos_response.status_code != 200:
-            print("Error: No se pudo acceder a la información del empleado o de las tareas.")
-            return
-
-        user_data = user_response.json()
-        todos_data = todos_response.json()
-
-        employee_name = user_data["name"]
-        total_tasks = len(todos_data)
-        done_tasks = sum(1 for task in todos_data if task["completed"])
-        
-        print(f"Empleado {employee_name} ha completado tareas ({done_tasks}/{total_tasks}):")
-        for task in todos_data:
-            if task["completed"]:
-                print(f"    {task['title']}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error de conexión: {e}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Uso: python script.py <employee_id>")
-    else:
-        employee_id = sys.argv[1]
-        get_employee_todo_progress(employee_id)
+    if len(sys.argv) < 2:
+        print(f"missing employee id as argument")
+        sys.exit(1)
+
+    URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
+
+    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todos",
+                                  params={"_expand": "user"})
+    data = EMPLOYEE_TODOS.json()
+
+    EMPLOYEE_NAME = data[0]["user"]["name"]
+    TOTAL_NUMBER_OF_TASKS = len(data)
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+    for task in data:
+        if task["completed"]:
+            NUMBER_OF_DONE_TASKS += 1
+            TASK_TITLE.append(task["title"])
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks"
+          f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for title in TASK_TITLE:
+        print("\t ", title)
